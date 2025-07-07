32b8ea80829b2175c6c3ed0866522a340672fcc1
barackwilliam <barackwilliam12@gmail.com>
2025-07-08 00:03:01 +0300

    contentNode added

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import ContentNode
+
+class ContentNodeAdmin(admin.ModelAdmin):
+    list_display = ('title_en', 'title_sw', 'node_type', 'level', 'parent')
+    search_fields = ('title_en', 'title_sw', 'node_type')
+    list_filter = ('node_type', 'level')
+    ordering = ('node_type', 'level')
+
+admin.site.register(ContentNode, ContentNodeAdmin)
